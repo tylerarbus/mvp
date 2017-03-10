99c4d7e58e4ce298df6eb00a12cbbc7c9ac53d03
Tyler Arbus <tyler.arbus@gmail.com>
2017-03-10 12:18:43 -0800

    Organization of basic app structure

--- server-config.js
+++ server-config.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
+var handler = require('./lib/request-handler.js');
 
 var app = express();
 
 app.use(bodyParser.json())
 app.use(express.static(__dirname + '/public'))
 app.use(cookieParser);
+
+app.get('/', handler.renderIndex);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var app = require('./server-config.js');
 
-var port = process.env.port || 8000;
+var port = process.env.port || 4888;
 
 app.listen(port);
 
